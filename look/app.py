4dc37c2474b63278ab7290604ae9e55c0c441e08
Yuske Fukuyama <yuskefuku@gmail.com>
2018-02-13 21:24:24 +0900

    core

--- look/app.py
+++ look/app.py
@@ -1,5 +1,19 @@
+import os
 import falcon
-# :) weee
 
-api = application = falcon.API()
+# :) weee
+from .images import ImageStore, Resource
+
+def create_app(image_store):
+    image_resource = Resource(image_store)
+    api = falcon.API()
+    api.add_route('/images', image_resource)
+    return api
+
+def get_app():
+    storage_path = os.environ.get('LOOK_STORAGE_PATH', '.')
+    image_store = ImageStore(storage_path)
+    return create_app(image_store)
+
+# api = application = falcon.API()
 
