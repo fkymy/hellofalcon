59e24375435dc8d74ce341a06a6814454868ca61
Yuske Fukuyama <yuskefuku@gmail.com>
2018-05-10 11:35:03 +0900

    add validation hooks and error handling

--- look/images.py
+++ look/images.py
@@ -1,5 +1,6 @@
 import os
 import io
+import re
 import uuid
 import mimetypes
 import json
@@ -7,7 +8,18 @@ import msgpack
 
 import falcon
 
-class Resource(object):
+ALLOWED_IMAGE_TYPES = (
+    'image/gif',
+    'image/jpeg',
+    'image/png',
+)
+
+def validate_image_type(req, resp, resource, params):
+    if req.content_type not in ALLOWED_IMAGE_TYPES:
+        msg = 'image type not allowed. must be png, jpeg, or gif'
+        raise falcon.HTTPBadRequest('Bad request', msg)
+
+class Collection(object):
 
     # the resource object must now be initialized with a path used during POST 
     def __init__(self, image_store):
@@ -33,15 +45,34 @@ class Resource(object):
         # illustrate how this may be overridden as needed.
         resp.status = falcon.HTTP_200
 
+    @falcon.before(validate_image_type)
     def on_post(self, req, resp):
         name = self._image_store.save(req.stream, req.content_type)
         resp.status = falcon.HTTP_201
         resp.location = '/images/' + name
 
 
+class Item(object):
+
+    def __init__(self, image_store):
+        self._image_store = image_store
+
+    def on_get(self, req, resp, name):
+        resp.content_type = mimetypes.guess_tyype(name)[0]
+
+        try:
+            resp.stream, resp.stream_len = self._image_store.open(name)
+        except IOError:
+            raise falcon.HttpNotFound()
+
+
 class ImageStore(object):
 
     _CHUNK_SIZE_BYTES = 4096
+    _IMAGE_NAME_PATTERN = re.compile(
+        '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.[a-z]{2,4}$'
+    )
+
 
     # note the use of dependency injection for standard library methods
     # to avoid monkey-patching.
@@ -65,4 +96,14 @@ class ImageStore(object):
 
         return name
 
+    def open(self, name):
+        # always validate untrusted input!
+        if not self._IMAGE_NAME_PATTERN.match(name):
+            raise IOError('File not found')
+
+        image_path = os.path.join(self._storage_path, name)
+        stream = self._fopen(image_path, 'rb')
+        stream_len = os.path.getsize(image_path)
+
+        return stream, stream_len
 
