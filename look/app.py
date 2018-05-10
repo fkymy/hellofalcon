a7ec23ee389542ad52aee33f8a079baf1e4c0c2a
Yuske Fukuyama <yuskefuku@gmail.com>
2018-05-10 09:30:13 +0900

    samuiyo

--- look/app.py
+++ look/app.py
@@ -1,19 +1,10 @@
-import os
 import falcon
 
+from .images import Resource
+
 # :) weee
-from .images import ImageStore, Resource
+api = application = falcon.API()
 
-def create_app(image_store):
-    image_resource = Resource(image_store)
-    api = falcon.API()
-    api.add_route('/images', image_resource)
-    return api
-
-def get_app():
-    storage_path = os.environ.get('LOOK_STORAGE_PATH', '.')
-    image_store = ImageStore(storage_path)
-    return create_app(image_store)
-
-# api = application = falcon.API()
+images = Resource()
+api.add_route('/images', images)
 
--- look/images.py
+++ look/images.py
@@ -1,21 +1,12 @@
-import io
-import os
-import re
-import uuid
-import mimetypes
-
-import falcon
+import json
 import msgpack
 
+import falcon
 
-class Collection(object):
+class Resource(object):
 
-    def __init__(self, image_store):
-        self._image_store = image_store
-
+    # responder methods
     def on_get(self, req, resp):
-        # TODO: Modify this to return a list of href's based on
-        # what images are actually available.
         doc = {
             'images': [
                 {
@@ -24,61 +15,13 @@ class Collection(object):
             ]
         }
 
+        # Create a json representation of the resource
+        # resp.body = json.dumps(doc, ensure_ascii=False)
         resp.data = msgpack.packb(doc, use_bin_type=True)
         resp.content_type = falcon.MEDIA_MSGPACK
+
+        # The following line can be omitted because 200 is the default
+        # status returned by the framework, but it is included here to
+        # illustrate how this may be overridden as needed.
         resp.status = falcon.HTTP_200
-
-    def on_post(self, req, resp):
-        name = self._image_store.save(req.stream, req.content_type)
-        resp.status = falcon.HTTP_201
-        resp.location = '/images/' + name
-
-
-class Item(object):
-
-    def __init__(self, image_store):
-        self._image_store = image_store
-
-    def on_get(self, req, resp, name):
-        resp.content_type = mimetypes.guess_type(name)[0]
-        resp.stream, resp.stream_len = self._image_store.open(name)
-
-
-class ImageStore(object):
-
-    _CHUNK_SIZE_BYTES = 4096
-    _IMAGE_NAME_PATTERN = re.compile(
-        '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.[a-z]{2,4}$'
-    )
-
-    def __init__(self, storage_path, uuidgen=uuid.uuid4, fopen=io.open):
-        self._storage_path = storage_path
-        self._uuidgen = uuidgen
-        self._fopen = fopen
-
-    def save(self, image_stream, image_content_type):
-        ext = mimetypes.guess_extension(image_content_type)
-        name = '{uuid}{ext}'.format(uuid=self._uuidgen(), ext=ext)
-        image_path = os.path.join(self._storage_path, name)
-
-        with self._fopen(image_path, 'wb') as image_file:
-            while True:
-                chunk = image_stream.read(self._CHUNK_SIZE_BYTES)
-                if not chunk:
-                    break
-
-                image_file.write(chunk)
-
-        return name
-
-    def open(self, name):
-        # Always validate untrusted input!
-        if not self._IMAGE_NAME_PATTERN.match(name):
-            raise IOError('File not found')
-
-        image_path = os.path.join(self._storage_path, name)
-        stream = self._fopen(image_path, 'rb')
-        stream_len = os.path.getsize(image_path)
-
-        return stream, stream_len
 
